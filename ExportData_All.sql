39f7f0a9a26fcf1d7cfcfbc8fc163d17a668163d
cretua <30320827+cretua@users.noreply.github.com>
2017-09-20 17:04:51 +0300

    Update ExportData_All.sql

--- ExportData_All.sql
+++ ExportData_All.sql
@@ -1,6 +1,8 @@
+SELECT * FROM testdata.productdata;
+
+/*
 SET @table_schema = 'testdata';
 SET @table_name = 'productdata';
 
 SELECT * FROM ', @table_schema, '.', @table_name,';
-
-/* SELECT * FROM testdata.productdata; */
+*/
