6b7cd120acd941f4dededb9cb9c9d3ce55a0162c
cretua <30320827+cretua@users.noreply.github.com>
2017-09-20 15:33:25 +0300

    Delete ExportData_Remote2.sql

--- ExportData_Remote2.sql
+++
@@ -1,8 +0,0 @@
-/* Change table_name, database_name 
-SET @table_name = 'test_table';
-SET @table_schema = 'sql8190040';
-
-SELECT * FROM ', @table_schema, '.', @table_name, ' */
-/*SELECT * FROM ', @table_schema, '.', @table_name, ' WHERE line = "standard"*/
-
-SELECT * FROM testdata.productdata;
